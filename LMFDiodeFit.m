e05457e30b3338d11ae69634c101668c35f30708
George <deft@archlinux.us>
2015-04-29 19:20:49 -0400

    can now fit reverse bias data, and better results from fsolve allows clamps to positive values

--- LMFDiodeFit.m
+++ LMFDiodeFit.m
@@ -8,7 +8,7 @@ Vth = kb*T/q;           % Thermal voltage. Unit: V
 
 
 % Plot boundaries & accuracy (used for plotting the model)
-Vmin = 0;
+Vmin = -1.0;
 Vmax = 1.4;
 Vstep = 0.01;
 
@@ -26,13 +26,13 @@ data_I = M(:,2).';
 
 
 % Initial guesses
-x(1) = 1 * 10^-4;     % Dark current
-x(2) = 1.0;           % Ideality factor
-x(3) = 2 * 10^-4;     % Series resistance
-x(4) = 1000000;      % Shunt resistance
+x(1) = 1 * 10^-2;     % Dark current (mA/cm^2)
+x(2) = 2.5;           % Ideality factor
+x(3) = 0.1;     % Series resistance  (kOhm*cm^2)
+x(4) = 1;      % Shunt resistance  (kOhm*cm^2)
 %x4 = 100;
-x(5) = 1 * 10^-4;     % Diode 2 dark current
-x(6) = 2.0;           % Diode 2 ideality
+%x(5) = 1 * 10^-2;     % Diode 2 dark current
+%x(6) = 2.0;           % Diode 2 ideality
 %x6 = 2.0;
 
 
@@ -52,10 +52,11 @@ x(6) = 2.0;           % Diode 2 ideality
     % More thorough weighting may provide a better fit for high series resistance. 
     %res = @(c) log(c(1)) + data_V.'/(c(2)*Vth) - guess_I.'*c(3)/(c(2)*Vth) - log(data_I.' - (data_V.' - guess_I.'*c(3))/c(4));
     %res = @(c) log(c(1)) + data_V.'/(c(2)*Vth) - guess_I.'*c(3)/(c(2)*Vth) - log(data_I.' - (data_V.' - guess_I.'*c(3))/x4);
-    res = @(c) [log(ImplicitDiodeF(c,data_V)).' - log(data_I.')
-        (c(4)<0)*c(4)*1000
-        (c(5)<0)*c(5)*1000
-        (c(6)<0)*c(6)*1000];
+    res = @(c) [log(abs(ImplicitDiodeF(c,data_V))).' - log(abs(data_I).')
+        (c(1)<0)*c(1)*1000000
+        (c(2)<0)*c(2)*100
+        (c(3)<0)*c(3)*100
+        (c(4)<0)*c(4)*100];
     %res = @(c) log(c(1) * exp((data_V.' - guess_I.'*c(3))/(c(2)*Vth)) + c(4) * exp((data_V.' - guess_I.'*c(3))/(x6*Vth))) - log(data_I.' - (data_V.' - guess_I.'*c(3))/x4);
 
 % LMF nonlinear least squares fit to the data with the guessed currents
@@ -63,7 +64,7 @@ x(6) = 2.0;           % Diode 2 ideality
 % MaxIter will improve accuracy at cost of compute time (default is 400)
     
 %x = LMFnlsq2(@(x) (log(ImplicitDiodeF(x,data_V)).' - log(data_I.')),x,'Display',0,'MaxIter',10);
-x = LMFnlsq2(res,x,'Display',0,'MaxIter',5);
+x = LMFnlsq2(res,x,'Display',[1,0],'MaxIter',30);
 %x = lsqcurvefit(@(x,dava_V) ImplicitDiodeF(x,data_V),x,data_V,data_I);
 
 
@@ -85,7 +86,7 @@ final_I = ImplicitDiodeF(x,V1);
 
 
 % plot for visual inspection
-semilogy(V1,final_I,'r')
+semilogy(V1,abs(final_I),'r')
 hold on
-semilogy(M(:,1),M(:,2),'b')
+semilogy(M(:,1),abs(M(:,2)),'b')
 hleg1 = legend('fit','data');
